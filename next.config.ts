c69a9dca0631d918f4b14e87fb6fc5a12861c188
dev-shrutiarora <shrutiarora125201@gmail.com>
2025-09-16 03:11:29 +0530

    final update:

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,31 @@
-import type { NextConfig } from 'next';
+// nect.config.js
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-      },
-    ],
-  },
-};
+output: 'export',
+reactStrictMode: true,
+images:{
+  unoptimized: true, //required for static exports
+},
+typescript:{
+  ignoreBuildErrors: false, // recommended to keep this false
+},
+//Enable if you're using styled-components
+compiler: {
+  styledComponents: true,
+},
 
-export default nextConfig;
+//Custom webpack configurations
+webpack(config){
+  //Add your custom webpack configurations here
+  return config
+},
+// Environment variables that should be availabe at build time
+env: {
+  MY_ENV_VAR: process.env.MY_ENV_VAR,
+},
+
+}
+
+export default nextConfig
